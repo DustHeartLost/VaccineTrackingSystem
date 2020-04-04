b3c0b2790c7476a04ef47fc9407c081fa1302c12
小白兔 <1132143964@qq.com>
2020-04-04 20:38:28 +0800

    带登陆页面，数据库，实体类的项目

---
+++ VaccineTrackingSystem/Models/Entity/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace helloworld.Models
+{
+    public class Role
+    {
+        /* id */
+        public int id;
+
+        /* 名称 */
+        public string name;
+
+        /***权限***/
+
+
+        /****备注***/
+        public string note;
+    }
+}
