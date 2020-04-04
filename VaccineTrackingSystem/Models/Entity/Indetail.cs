86fa6642bd336389e632f36af3b3526e58b3ceb4
unknown <1967530737@qq.com>
2020-04-04 11:40:46 +0800

    20200404

---
+++ VaccineTrackingSystem/Models/Entity/Indetail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace helloworld.Models
+{
+    public class Indetail
+    {
+        /* id */
+        public int id;
+
+        /* 品类编码 */
+        public string num;
+
+        /***权限***/
+        public string name;
+
+        /****类别***/
+        public string kind;
+
+        /* 单位 */
+        public string unit;
+
+        /***规格***/
+        public string spec;
+
+        /****生产厂家***/
+        public string factory;
+
+        /****备注***/
+        public string note;
+
+    }
+}
