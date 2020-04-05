e1e80ce5158b81c243b8acc199601dd1e4dd750e
小白兔 <1132143964@qq.com>
2020-04-05 23:16:38 +0800

    登录 主页面完成

---
+++ VaccineTrackingSystem/Models/DAL/CategoryDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VaccineTrackingSystem.Models.DAL
+{
+    public class CategoryDAL
+    {
+
+
+    }
+}
---
+++ VaccineTrackingSystem/Models/Entity/Inflow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VaccineTrackingSystem.Models
+{
+    public class Inflow
+    {
+        /* id */
+        public int id;
+
+        /* 药品编码 */
+        public string cagNum;
+
+        /***关联库房编号***/
+        public int storeID;
+
+        /****入库时间***/
+        public string date;
+
+        /* 关联员工编号 */
+        public string userNum;
+
+        /***数量***/
+        public int quantity;
+
+        /****单价***/
+        public decimal price;
+
+        /****批号***/
+        public string batchNum;
+
+        public Inflow(int id, string cagNum, int storeID, string date, string userNum, int quantity, int price, string batchNum)
+        {
+            this.id = id;
+            this.cagNum = cagNum;
+            this.storeID = storeID;
+            this.date = date;
+            this.userNum = userNum;
+            this.quantity = quantity;
+            this.price = price;
+            this.batchNum = batchNum;
+        }
+    }
+}
