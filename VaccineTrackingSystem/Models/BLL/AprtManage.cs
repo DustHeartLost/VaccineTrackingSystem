2894dbf2d7fe7741b1ba26d1428d3e392b53b913
小白兔 <1132143964@qq.com>
2020-04-06 12:58:29 +0800

    提交一次准备push

---
+++ VaccineTrackingSystem/Models/BLL/AprtManage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VaccineTrackingSystem.Models.DAL;
+
+namespace VaccineTrackingSystem.Models.BLL
+{
+    public class AprtManage
+    {
+        static public bool Add(Apartment apartment,string msg)
+        {
+            if(ApartmentDAL.Add(apartment, msg))
+                return true;
+            return false;
+        }
+        /**static public bool Delete(int id, string msg)
+        {
+            if (ApartmentDAL.Delete(id, msg))
+                return true;
+            return false;
+        }*/
+        static public bool Update(Apartment apartment,string msg)
+        {
+            if (ApartmentDAL.Update(apartment, msg))
+                return true;
+            return false;
+        }
+        static public Apartment Query(string num, string msg)
+        {
+            if (ApartmentDAL.Query(num, msg) == null)
+                return null;
+            return ApartmentDAL.Query(num, msg);
+        }
+        static public List<Apartment> QueryAll(string msg)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ VaccineTrackingSystem/VIew/test.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using VaccineTrackingSystem.Models;
+using VaccineTrackingSystem.Models.DAL;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            /*测试outflow查询代码*/
+            //string msg = "";
+            //Outflow list = OutflowDAL.Query(2,msg);
+            //if (list == null)
+            //{
+            //    Label1.Text = msg;
+            //}
+            //else {
+            //    Label1.Text = list.date;
+            //}
+            /*测试outflow插入代码*/
+            //string msg = "";
+            //Outflow outflow = new Outflow(1,"23",2,"2020-04-15","456",20,12.8m,"2020-04-25","正常出库");
+            //if (OutflowDAL.Add(outflow, msg)) { msg = "插入成功"; };
+            //Label1.Text = msg;
+
+            /*测试indetail代码*/
+            string msg="";
+            Indetail indetail=IndetailDAL.Query(1,msg);
+            if (indetail == null)
+            {
+                Label1.Text = msg;
+            }
+            else {
+                Label1.Text = indetail.name;
+            }
+        }
+    }
+}
