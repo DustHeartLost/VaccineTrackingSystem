71fe65ef37e37ef259c40b342f5db64466e3d5ae
小白兔 <1132143964@qq.com>
2020-04-08 19:52:32 +0800

    修改了一个文件名字

--- VaccineTrackingSystem/VIew/Module/RoleManage/Role.aspx
+++ VaccineTrackingSystem/VIew/Module/RoleManage/Role.aspx
@@ -3,7 +3,7 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="CurrentList" runat="server">
 </asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:Content ID="Content3" ContentPlaceHolderID="Search" runat="server">
 </asp:Content>
 <asp:Content ID="Content4" ContentPlaceHolderID="Other" runat="server">
 </asp:Content>
--- VaccineTrackingSystem/VIew/Template/Main.Master.designer.cs
+++ VaccineTrackingSystem/VIew/Template/Main.Master.designer.cs
@@ -42,13 +42,13 @@ namespace VaccineTrackingSystem.VIew.Template
         protected global::System.Web.UI.WebControls.ContentPlaceHolder CurrentList;
 
         /// <summary>
-        /// ContentPlaceHolder1 控件。
+        /// Search 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Search;
 
         /// <summary>
         /// Other 控件。
