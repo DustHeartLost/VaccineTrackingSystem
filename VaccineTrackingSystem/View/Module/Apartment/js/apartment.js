b6eeb2a44b870d499e7c4a8d73f53dffa1377342
Administrator <1967530737@qq.com>
2020-04-11 09:16:26 +0800

    正在实现apartment增加

--- VaccineTrackingSystem/View/Module/Apartment/js/apartment.js
+++ VaccineTrackingSystem/View/Module/Apartment/js/apartment.js
@@ -2,12 +2,20 @@
     var data = JSON.parse(temp);
     var html = "";
     for (var i = 0; i < data.length; i++) {
-        html += "<tr class=\"dataRow\" style=\"height:60px\"><td>" + data[i].id + "</td><td>" + data[i].num + "</td><td>" + data[i].name + "</td><td>";
-        html += data[i].note + "</td></tr>";
+        if (i % 2 == 0) {
+            html += "<tr class=\"dataRow\" style=\"height:50px\"><td>" + data[i].id + "</td><td>" + data[i].num + "</td><td>" + data[i].name + "</td><td>";
+            html += data[i].note + "</td></tr>";
+        }
+        else {
+            html += "<tr class=\"dataRow2\" style=\"height:50px\"><td>" + data[i].id + "</td><td>" + data[i].num + "</td><td>" + data[i].name + "</td><td>";
+            html += data[i].note + "</td></tr>";
+        }
+       
     }
     $("#caption").after(html);
 
 }
+
 
 $(document).ready(function () {
     $("#tableContainer").delegate(".dataRow", "mouseenter", function () {
@@ -16,13 +24,27 @@ $(document).ready(function () {
     $("#tableContainer").delegate(".dataRow", "mouseleave", function () {
         $(this).removeClass("tr-mouseover");
     });
+    $("#tableContainer").delegate(".dataRow2", "mouseenter", function () {
+        $(this).addClass("tr-mouseover");
+    });
+    $("#tableContainer").delegate(".dataRow2", "mouseleave", function () {
+        $(this).removeClass("tr-mouseover");
+    });
 });
 
 function clear() {
     $("tr").remove(".dataRow");
+    $("tr").remove(".dataRow2");
 }
 
-function reCreateTable(temp, extra){
+
+
+function reCreateTable(temp){
     clear();
     createTable(temp);
 } 
+
+function addRecord() {
+    html = "<tr class=\"dataRow\" style=\"height:50px\"><td> </td><td contentEditable= \"true \"> </td><td> </td><td> </td></tr>";
+    
+}
