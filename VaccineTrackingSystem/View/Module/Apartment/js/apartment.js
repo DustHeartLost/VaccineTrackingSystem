7915be2e35838a32027de74d16ef3f59bfee5ead
Administrator <1967530737@qq.com>
2020-04-12 09:49:52 +0800

    apartment role 更新 添加完成

--- VaccineTrackingSystem/View/Module/Apartment/js/apartment.js
+++ VaccineTrackingSystem/View/Module/Apartment/js/apartment.js
@@ -10,7 +10,6 @@
             html += "<tr class=\"dataRow2\" style=\"height:50px\"><td>" + data[i].id + "</td><td>" + data[i].num + "</td><td>" + data[i].name + "</td><td>";
             html += data[i].note + "</td></tr>";
         }
-       
     }
     $("#caption").after(html);
 
@@ -47,21 +46,8 @@ function reCreateTable(temp){
 
 
 function addRecord() { 
-    html = "<tr id=\"newOne\" class=\"dataRow3\"  style=\"height:50px;width:130px\"><td>请依次填充id之后的内容 </td><td class=\"dbclicktd\"> </td><td class=\"dbclicktd\"> </td><td class=\"dbclicktd\"> </td></tr>";
+    html = "<tr id=\"newOne\" class=\"dataRow3\"  style=\"height:50px;width:130px\"><td>请依次填充id之后的内容 </td><td contentEditable=\"true\" class=\"dbclicktd\"> </td><td contentEditable=\"true\" class=\"dbclicktd\"> </td><td contentEditable=\"true\" class=\"dbclicktd\"> </td></tr>";
     $("#caption").after(html);
-    $(".dataRow3").find(".dbclicktd").bind("click", function () {
-        var inputNum = "<input type='text' id= 'aptNum' style='height:35px;' value=" + $(this).text() + " >";
-        $(this).text("");
-        $(this).append(inputNum);
-        $("#aptNum").focus();
-        $("input").blur(function () {
-            if ($(this).val() == "") {
-                $(this).remove();
-            } else {
-                $(this).closest("td").text($(this).val());
-            }
-        });
-    });
 }
 
 
