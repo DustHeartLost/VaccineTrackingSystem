2847de31ea927be8d4184ff228c23ee32b855101
小白兔 <1132143964@qq.com>
2020-04-14 23:30:16 +0800

    销毁处理增加了一下权限判断

--- VaccineTrackingSystem/View/Module/Destory/Destory.aspx.cs
+++ VaccineTrackingSystem/View/Module/Destory/Destory.aspx.cs
@@ -16,14 +16,29 @@ namespace VaccineTrackingSystem.View.Module.Destory
     {
         protected static int totalPage;
         protected static int currentPage;
-        protected static int stockID = 3;
-        protected static string num="123";//员工编号
+        protected static int storeID;
+        protected static string num;//员工编号
 
         protected void Page_Load(object sender, EventArgs e)
         {
             totalPage = 0;
             currentPage = -1;
             //stockID = int.Parse(Request.QueryString["stockID"]);
+            if (HttpContext.Current.Session["user"] == null)
+                Response.Write("<script language='javascript'>alert('登录信息过期，请重新登录');location.href='../../Login/Login.aspx'</script>");
+            else
+            {
+                Dictionary<string, string> user = HttpContext.Current.Session["user"] as Dictionary<string, string>;
+                num=user["num"].ToString();
+                try
+                {
+                    storeID = int.Parse(user["storeID"]);
+                }
+                catch
+                {
+                    storeID = -1;
+                };
+            }
         }
 
         [WebMethod]
@@ -34,14 +49,14 @@ namespace VaccineTrackingSystem.View.Module.Destory
                 currentPage--;
             }
             string msg;
-            string jsonData = JsonConvert.SerializeObject(Models.BLL.DestoryManage.Query(stockID, ref totalPage, ref currentPage, out msg));
+            string jsonData = JsonConvert.SerializeObject(Models.BLL.DestoryManage.Query(storeID, ref totalPage, ref currentPage, out msg));
             return jsonData != null ? JsonConvert.SerializeObject(new Packet(200, jsonData, $"{totalPage + 1}+{currentPage + 1}")) : JsonConvert.SerializeObject(new Packet(201, msg));
         }
         [WebMethod]
         public static string GetDown()
         {
             string msg;
-            string jsonData = JsonConvert.SerializeObject(Models.BLL.DestoryManage.Query(stockID, ref totalPage, ref currentPage, out msg));
+            string jsonData = JsonConvert.SerializeObject(Models.BLL.DestoryManage.Query(storeID, ref totalPage, ref currentPage, out msg));
             return jsonData != null ? JsonConvert.SerializeObject(new Packet(200, jsonData, $"{totalPage + 1}+{currentPage + 1}")) : JsonConvert.SerializeObject(new Packet(201, msg));
         }
         [WebMethod]
@@ -50,14 +65,14 @@ namespace VaccineTrackingSystem.View.Module.Destory
             if (currentPage == -1 || currentPage == 0) return JsonConvert.SerializeObject(new Packet(201, "没有记录"));
             currentPage -= 2;
             string msg;
-            string jsonData = JsonConvert.SerializeObject(Models.BLL.DestoryManage.Query(stockID, ref totalPage, ref currentPage, out msg));
+            string jsonData = JsonConvert.SerializeObject(Models.BLL.DestoryManage.Query(storeID, ref totalPage, ref currentPage, out msg));
             return jsonData != null ? JsonConvert.SerializeObject(new Packet(200, jsonData, $"{totalPage + 1}+{currentPage + 1}")) : JsonConvert.SerializeObject(new Packet(201, msg));
         }
         [WebMethod]
         public static string DestoryRecord()
         {
             string msg1;
-            List<Indetail> list = Models.BLL.DestoryManage.Query(stockID, ref totalPage, ref currentPage, out msg1);
+            List<Indetail> list = Models.BLL.DestoryManage.Query(storeID, ref totalPage, ref currentPage, out msg1);
             if (list == null) {
                 return JsonConvert.SerializeObject(new Packet(201, msg1));
             }
