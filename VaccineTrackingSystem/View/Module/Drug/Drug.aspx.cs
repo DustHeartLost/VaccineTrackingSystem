6aed9a2259463483bec818cd4537b6722f4ee7c3
Administrator <1967530737@qq.com>
2020-05-09 22:54:15 +0800

    药品类别销毁

--- VaccineTrackingSystem/View/Module/Drug/Drug.aspx.cs
+++ VaccineTrackingSystem/View/Module/Drug/Drug.aspx.cs
@@ -4,9 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using VaccineTrackingSystem.Models.Entity;
+
 
 namespace VaccineTrackingSystem.View.Module.Drug
 {
@@ -76,6 +75,27 @@ namespace VaccineTrackingSystem.View.Module.Drug
             currentPage = 0;
             string jsonData = Models.BLL.DrugManage.Query(temp, out msg);
             return jsonData != null ? JsonConvert.SerializeObject(new Packet(200, jsonData, $"{totalPage + 1}+{currentPage + 1}")) : JsonConvert.SerializeObject(new Packet(201, msg));
+        }
+
+        [System.Web.Services.WebMethod]
+        public static string DestoryAllRecord()
+        {
+            string msg1;
+            string temp = Models.BLL.DrugManage.QueryDesAll(out msg1);
+            if (temp == null)
+            {
+                return JsonConvert.SerializeObject(new Packet(201, msg1));
+            }
+            string msg;
+            List<Models.Entity.Drug> list = JsonConvert.DeserializeObject<List<Models.Entity.Drug>>(temp);
+            return Models.BLL.DrugManage.Destory(list,out msg) ? JsonConvert.SerializeObject(new Packet(200, null)) : JsonConvert.SerializeObject(new Packet(202, msg));
+        }
+        [System.Web.Services.WebMethod]
+        public static string DestoryRecord(string temp)
+        {
+            List<Models.Entity.Drug> list = JsonConvert.DeserializeObject<List<Models.Entity.Drug>>(temp);
+            string msg;
+            return Models.BLL.DrugManage.Destory(list,out msg) ? JsonConvert.SerializeObject(new Packet(200, Models.BLL.DrugManage.QueryAll(out msg, ref totalPage, ref currentPage), $"{totalPage + 1}+{currentPage + 1}")) : JsonConvert.SerializeObject(new Packet(202, msg));
         }
     }
 }
--- VaccineTrackingSystem/View/Module/Drug/js/drug.js
+++ VaccineTrackingSystem/View/Module/Drug/js/drug.js
@@ -5,7 +5,7 @@
         if (i % 2 == 0)
             html += "<tr class=\"dataRow\" style=\"height:50px\"><td><input class=\"checkBox\"  type=\"checkbox\"  style=\"padding: 5px;\" onclick=\"clickCheck(this)\"></td><td class=\"ID\">" + data[i].id + "</td><td class=\"editTd\">" + data[i].kind + "</td></tr>";
         else
-            html += "<tr class=\"dataRow2\" style=\"height:50px\"><td><input class=\"checkBox\"  type=\"checkbox\"  style=\"padding: 5px;\" onclick=\"clickCheck(this)\"></td><td class=\"ID\">" + data[i].id + "</td><td class=\"editTd\">" + data[i].kind + "</td></tr>";
+            html += "<tr class=\"dataRow2\" style=\"height:50px\"><td><input class=\"checkBox\" type=\"checkbox\"  style=\"padding: 5px;\" onclick=\"clickCheck(this)\"></td><td class=\"ID\">" + data[i].id + "</td><td class=\"editTd\">" + data[i].kind + "</td></tr>";
     }
     $("#caption").after(html);
     var x = extra.split('+');
@@ -15,6 +15,7 @@
     $("#showAll").hide();
 }
 
+var global;
 
 $(document).ready(function () {
     $("#tableContainer").delegate(".dataRow", "mouseenter", function () {
@@ -42,6 +43,7 @@ function reCreateTable(temp, extra) {
 }
 
 function showCheckBox() {
+    global = 1;
     $(".checkBox").show();
     $("#cancelUpdate").show();
     $("#confirmUpdate").show();
@@ -55,6 +57,10 @@ function showCheckBox() {
     $("#concelAdd").hide();
     $("#confirmAdd").hide();
     $("#add").hide();
+
+    $("#dfunct").hide();
+    $("#destory").hide();
+    $("#destoryAll").hide();
 }
 
 function cancelUpdate() {
@@ -73,6 +79,10 @@ function cancelUpdate() {
     $("#concelAdd").hide();
     $("#confirmAdd").hide();
     $("#add").show();
+
+    $("#dfunct").show();
+    $("#destory").hide();
+    $("#destoryAll").hide();
 
     clear();
     $.ajax({
@@ -129,28 +139,30 @@ function confirmUpdate() {
 }
 
 function clickCheck(obj) {
-    $(":checkbox").each(function () {
-        $(this).prop("checked", false);   //选中，不选中 是false        
-    });
-    $(obj).prop("checked", true);
+    if (global == 1) {
+        $(":checkbox").each(function () {
+            $(this).prop("checked", false);   //选中，不选中 是false        
+        });
+        $(obj).prop("checked", true);
 
-    $(".edit").each(function () {
-        $(this).attr("disabled", true);
-    });
-    $(".editTd").closest("tr").find("td").each(function () {
-        $(this).attr("contenteditable", false);
-    });
+        $(".edit").each(function () {
+            $(this).attr("disabled", true);
+        });
+        $(".editTd").closest("tr").find("td").each(function () {
+            $(this).attr("contenteditable", false);
+        });
 
-    $(obj).closest("tr").find("select").each(function () {
-        $(this).attr("disabled", false);
-    });
-    $(obj).closest("tr").find("td.editTd").each(function () {
-        $(this).attr("contenteditable", true);
-    });
+        $(obj).closest("tr").find("select").each(function () {
+            $(this).attr("disabled", false);
+        });
+        $(obj).closest("tr").find("td.editTd").each(function () {
+            $(this).attr("contenteditable", true);
+        });
 
-    $(obj).closest("tr").find("#ban").each(function () {
-        $(this).attr("contenteditable", false);
-    });
+        $(obj).closest("tr").find("#ban").each(function () {
+            $(this).attr("contenteditable", false);
+        });
+    }
 
 }
 
@@ -202,6 +214,10 @@ function add() {
     $("#confirmUpdate").hide();
     $("#update").hide();
 
+    $("#dfunct").hide();
+    $("#destory").hide();
+    $("#destoryAll").hide();
+
     clear();
     addRecord();
 }
@@ -219,6 +235,10 @@ function concelAdd() {
     $("#add").show();
     $("#confirmAdd").hide();
     $("#concelAdd").hide();
+
+    $("#dfunct").show();
+    $("#destory").hide();
+    $("#destoryAll").hide();
 
     clear();
     $.ajax({
@@ -310,6 +330,11 @@ function search() {
                     $("#cancelUpdate").hide();
                     $("#confirmUpdate").hide();
                     $("#update").show();
+
+                    $("#dfunct").hide();
+                    $("#destoryAll").hide();
+                    $("#destory").show();
+                    $(".checkBox").show();
                 }
                 else {
                     alert(tempT.data);
@@ -326,4 +351,105 @@ function search() {
 
 function showAll() {
     concelAdd();
+}
+
+
+function destoryAll() {
+    $.ajax({
+        type: "post", //要用post方式                 
+        url: "Drug.aspx/DestoryAllRecord",//方法所在页面和方法名
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            var temp = JSON.parse(data.d);//返回的数据用data.d获取内容
+            if (temp.code == 200) { clear(); alert("销毁成功"); }
+            else alert(temp.data);
+        },
+        error: function (err) {
+            alert(err);
+        }
+    });
+}
+
+function dfunct() {
+    global = 0;
+    $("#dfunct").hide();
+    $(".checkBox").show();
+    $("#cancelUpdate").hide();
+    $("#confirmUpdate").hide();
+    $("#update").hide();
+
+    $("#down").hide();
+    $("#up").hide();
+    $("#current").hide();
+    $("#total").hide();
+
+    $("#concelAdd").hide();
+    $("#confirmAdd").hide();
+    $("#add").hide();
+
+    $("#destory").show();
+
+    $("#destoryAll").show();
+
+    $("#showAll").show();
+}
+
+
+
+function destory() {
+    var list = [];
+    global = 0;
+    $(".checkBox").show();
+    $(":checkbox").each(function () {
+        if ($(this).prop("checked")) {
+
+            var temp = $(this).closest("tr").find("td.ID").text() + "@@";
+            $(this).closest("tr").find("td.editTd").each(function () {
+                temp += $(this).text() + "@@";
+            });
+            temp1 = temp.split("@@");
+            if (temp1[1] == "") {
+                alert("请输入药品类别");
+                return;
+            };
+
+            var obj = new Object();
+            obj.id = temp1[0];
+            obj.kind = temp1[1];
+            list.push(obj);
+        }
+    });
+    if (list.length == 0) {
+        return;
+    }
+    $.ajax({
+        type: "post", //要用post方式                 
+        url: "Drug.aspx/DestoryRecord",//方法所在页面和方法名
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: "{'temp':'" + JSON.stringify(list) + "'}",
+        success: function (data) {
+            var temp = JSON.parse(data.d);//返回的数据用data.d获取内容
+            if (temp.code == 200) {
+                alert("销毁成功");
+                clear();
+                if (temp.data != "null" && temp.extra != "null")
+                {
+                    createTable(temp.data, temp.extra);
+                    $("#showAll").show();
+                    $("#destory").show();
+                    $("#destoryAll").show();
+                }
+                else {
+                    $("#total").text("共0页");
+                    $("#current").text("第0页");
+                }
+            }
+            else alert(temp.data);
+        },
+        error: function (err) {
+            alert(err);
+        }
+    });
 }
